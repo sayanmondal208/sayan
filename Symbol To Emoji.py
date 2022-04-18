1d0bd1dc6821df9aaa5b9d2cf855de8b59e767f5
sayanmondal208 <64949395+sayanmondal208@users.noreply.github.com>
2022-04-18 19:39:34 +0530

    Add files via upload

---
+++ Symbol To Emoji.py
@@ -0,0 +1,15 @@
+while True:
+    message = input("> ")
+    words = message.split( " ")
+    emojis = {
+       ":)" : "😀",
+       ":(" : "😞",
+       "lol" : "😂",
+       "sick":"😨",
+       "happy": "😀",
+       "mermaid": "🧜‍"
+    }
+    outcome = " "
+    for word in words:
+       outcome += emojis.get(word, word) + " "
+    print(outcome)
